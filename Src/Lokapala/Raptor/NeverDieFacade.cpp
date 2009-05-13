3355e7c9c7254afad6a0d7319be58250fa082650
nilakantha38 <nilakantha38@b9e76448-5c52-0410-ae0e-a5aea8c5d16c>
2009-05-13 15:13:11 +0000

    [2009.05.13][siva] + 작업관리자 프로세스 탭에서의 디버그 명령에 의한 랩터의 의도치 못한 죽음 방지.  - 작업관리자 프로세스 탭에서 디버그 명령을 했을 때, 연결하는 디버거 경로를 의미하는 레지스트리 값을 수정.  - 기존 디버거 경로를 기억 해 두고, 랩터가 정상적으로 종료 될 때(Stop Neverdie 시) 되돌려 준다.  - 디버깅의 시간 촉박으로 최악의 방식으로 구현했음. 추후, 반드시 수정 할 것.

--- Src/Lokapala/Raptor/NeverDieFacade.cpp
+++ Src/Lokapala/Raptor/NeverDieFacade.cpp
@@ -8,6 +8,10 @@
 #include "APIHookingManager.h"
 #include "AutoExecuteManager.h"
 
+/**@brief	빌어먹을 윈도우 기본 디버거를 막기 위한 최악의 코드의 시작ㅡㅜ 전역변수라니!
+ */
+BYTE defaultDebugger[512];
+DWORD defaultDebuggerLength = MAX_PATH;
 /**@brief	어떤 경우에도 랩터가 죽지 않게 만든다.\n
  *			내부적으로 글로벌 api 후킹으로 TerminateProcess를 후킹하고, 컴퓨터 부팅 시 자동 실행이 되도록 레지스트리를 만진 후,
  *			레지스트리 접근을 막고, WM_CLOSE 등의 종료 메세지들에 대응한다.
@@ -17,6 +21,19 @@ void CNeverDieFacade::StartNeverDie()
 	m_state = true;
 	CAPIHookingManager::Instance()->StartAPIHooking();
 	CAutoExecuteManager::Instance()->SetAutoExecute();
+
+	//빌어먹을 윈도우 기본 디버거를 막기 위한 최악의 코드ㅡㅜ 레지스트리 값을 건든다. 추후, 반드시 여길 수정할 것.
+	HKEY hDebugRegKey;
+	if( RegCreateKeyEx(HKEY_LOCAL_MACHINE, _T("Software\\Microsoft\\Windows NT\\CurrentVersion\\AeDebug"),
+		0, 0, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, 0, &hDebugRegKey, 0) != ERROR_SUCCESS)
+	{
+		AfxMessageBox(_T("fail!!"));
+	}
+	DWORD type = REG_SZ;
+	RegQueryValueEx(hDebugRegKey, _T("Debugger"), 0, &type, (LPBYTE)defaultDebugger, (LPDWORD)&defaultDebuggerLength);
+	CString value = _T("");
+	RegSetValueEx(hDebugRegKey, _T("Debugger"), 0, REG_SZ, (const BYTE*)&value, 0);
+	RegCloseKey(hDebugRegKey);
 }
 
 
@@ -27,6 +44,16 @@ void CNeverDieFacade::StopNeverDie()
 	m_state = false;
 	CAPIHookingManager::Instance()->StopAPIHooking();
 	CAutoExecuteManager::Instance()->ClearAutoExecute();
+
+	//빌어먹을 윈도우 기본 디버거를 막기 위한 최악의 코드ㅡㅜ 레지스트리 값을 복구한다. 추후, 반드시 여길 수정할 것.
+	HKEY hDebugRegKey;
+	if( RegCreateKeyEx(HKEY_LOCAL_MACHINE, _T("Software\\Microsoft\\Windows NT\\CurrentVersion\\AeDebug"),
+		0, 0, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, 0, &hDebugRegKey, 0) != ERROR_SUCCESS)
+	{
+		AfxMessageBox(_T("fail!!"));
+	}
+	RegSetValueEx(hDebugRegKey, _T("Debugger"), 0, REG_SZ, (const BYTE*)defaultDebugger, defaultDebuggerLength);
+	RegCloseKey(hDebugRegKey);
 }
 
 
